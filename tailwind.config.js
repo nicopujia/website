e46f22e7c2fbd50c4fb1d252153d763995e2ad00
Nico Pujia <nicobatman0@gmail.com>
2023-07-15 23:22:47 -0300

    Simplified website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./build/js/*.js"],
+  content: ["./build/*.html"],
   theme: {
-    extend: {
-      keyframes: {
-        "open-menu": {
-          "0%": { transform: "scaleY(0)" },
-          "100%": { transform: "scaleY(1)" },
-        },
-      },
-      animation: {
-        "open-menu": "open-menu 0.25s ease-in-out forwards",
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
