5c8588a17080f95f4539c022efcfd7a853f12062
Nicolás Pujia <nicopujia.dev@gmail.com>
2025-04-13 15:43:24 -0300

    Fix theme

--- src/static/js/theme.js
+++ src/static/js/theme.js
@@ -1,53 +1,65 @@
-const storageKey = 'theme'
-const lightTheme = 'light'
-const darkTheme = 'dark'
-const themeBtn = document.getElementById('theme-btn')
-const autoIcon = themeBtn.children.item(0)
-const darkIcon = themeBtn.children.item(1)
-const lightIcon = themeBtn.children.item(2)
+const storageKey = "theme";
+const lightTheme = "light";
+const darkTheme = "dark";
+const systemTheme = "system";
+const themeBtn = document.getElementById("theme-btn");
+const autoIcon = themeBtn.children.item(0);
+const darkIcon = themeBtn.children.item(1);
+const lightIcon = themeBtn.children.item(2);
 
 function setDarkTheme(enabled) {
-    document.documentElement.classList.toggle('dark', enabled)
+  document.documentElement.classList.toggle("dark", enabled);
 }
 
 function showSavedTheme() {
-    let currentIcon
+  let currentIcon;
 
-    switch (localStorage.getItem(storageKey)) {
-        case lightTheme:
-            currentIcon = lightIcon
-            setDarkTheme(false)
-            break;
-        case darkTheme:
-            currentIcon = darkIcon
-            setDarkTheme(true)
-            break;
-        default:
-            currentIcon = autoIcon
-            setDarkTheme(window.matchMedia('(prefers-color-scheme: dark)').matches)
-            break;
-    }
+  switch (localStorage.getItem(storageKey)) {
+    case lightTheme:
+      currentIcon = lightIcon;
+      setDarkTheme(false);
+      break;
+    case darkTheme:
+      currentIcon = darkIcon;
+      setDarkTheme(true);
+      break;
+    default:
+      currentIcon = autoIcon;
+      setDarkTheme(window.matchMedia("(prefers-color-scheme: dark)").matches);
+      break;
+  }
 
-    for (const icon of themeBtn.children) {
-        icon.classList.add('hidden')
-    }
+  for (const icon of themeBtn.children) {
+    icon.classList.add("hidden");
+  }
 
-    currentIcon.classList.remove('hidden')
+  currentIcon.classList.remove("hidden");
 }
 
-themeBtn.addEventListener('click', () => {
-    switch (localStorage.getItem(storageKey)) {
-        case lightTheme:
-            localStorage.setItem(storageKey, null)
-            break;
-        case darkTheme:
-            localStorage.setItem(storageKey, lightTheme)
-            break;
-        default:
-            localStorage.setItem(storageKey, darkTheme)
-            break;
-    }
-    showSavedTheme()
-})
+const darkModeMediaQuery = window.matchMedia("(prefers-color-scheme: dark)");
+darkModeMediaQuery.addEventListener("change", (e) => {
+  console.log(
+    "localStorage.getItem(storageKey)",
+    localStorage.getItem(storageKey)
+  );
+  if (localStorage.getItem(storageKey) === systemTheme) {
+    setDarkTheme(e.matches);
+  }
+});
 
-showSavedTheme()
+themeBtn.addEventListener("click", () => {
+  switch (localStorage.getItem(storageKey)) {
+    case lightTheme:
+      localStorage.setItem(storageKey, systemTheme);
+      break;
+    case darkTheme:
+      localStorage.setItem(storageKey, lightTheme);
+      break;
+    default:
+      localStorage.setItem(storageKey, darkTheme);
+      break;
+  }
+  showSavedTheme();
+});
+
+showSavedTheme();
